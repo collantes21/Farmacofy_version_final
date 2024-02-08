a24a8b89e13deee2fc1da219b2a5e9c7309ffda1
collantes21 <alvaritocollantes21@hotmail.com>
2024-02-08 23:44:41 +0100

    arreglo flecha pa atras menu drawer pantalla registro login

--- lib/inicioSesion/pantallaRegistroLogin.dart
+++ lib/inicioSesion/pantallaRegistroLogin.dart
@@ -103,86 +103,82 @@ class _InicioPantallaState extends State<InicioPantalla> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FarmacoFy'),
-        backgroundColor: const Color(0xFF02A724),
-        flexibleSpace: Container( //Sirve para definir el color de la barra de estado
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  
-                  Color(0xFF02A724),
-                  Color.fromARGB(255, 18, 240, 63),
-                  Color.fromARGB(255, 11, 134, 34),
-                  
-                ],
-              ),
-            ),
-          ),
-        centerTitle: true,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.white, // Puedes cambiar esto a cualquier color que desees
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'FarmacoFy',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegistroPantalla()),
-                  );
-                },
-                child: const Text('Registro'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPantalla()),
-                  );
-                },
-                child: const Text('Login'),
-              ),
-              (hayCaida) ? ElevatedButton(
-                  onPressed: ()async {await llamaEmergencias();}, //Asignamos la funcion llamaEmergencias al boton
-                  style: ElevatedButton.styleFrom(
-                          primary: Colors.red,
-                          onPrimary: Colors.white,
-                          onSurface: Colors.grey,
-                  ),
-                  child: Text(
-                            'Avisar a Emergencias',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                  ),))  : Text(''),
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('FarmacoFy'),
+      backgroundColor: const Color(0xFF02A724),
+      flexibleSpace: Container( //Sirve para definir el color de la barra de estado
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF02A724),
+              Color.fromARGB(255, 18, 240, 63),
+              Color.fromARGB(255, 11, 134, 34),
             ],
           ),
         ),
       ),
-    );
-    
-  }
+      centerTitle: true,
+      automaticallyImplyLeading: false, // Evita que aparezca la flecha de regreso
+    ),
+    body: Container(
+      decoration: BoxDecoration(
+        color: Colors.white, // Puedes cambiar esto a cualquier color que desees
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'FarmacoFy',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistroPantalla()),
+                );
+              },
+              child: const Text('Registro'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPantalla()),
+                );
+              },
+              child: const Text('Login'),
+            ),
+            (hayCaida) ? ElevatedButton(
+              onPressed: () async {
+                await llamaEmergencias();
+              }, //Asignamos la funcion llamaEmergencias al boton
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                onPrimary: Colors.white,
+                onSurface: Colors.grey,
+              ),
+              child: Text(
+                'Avisar a Emergencias',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ) : Text(''),
+          ],
+        ),
+      ),
+    ),
+  );
 }
-
-void main() {
-  runApp(const MaterialApp(
-    home: InicioPantalla(),
-  ));
 }
--- lib/presentacion/widgets/menu_drawer.dart
+++ lib/presentacion/widgets/menu_drawer.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:farmacofy/inicioSesion/pantallaLogin.dart';
 import 'package:farmacofy/inicioSesion/pantallaRegistroLogin.dart';
 import 'package:farmacofy/models/consulta_medica.dart';
@@ -16,198 +14,156 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MenuDrawer extends StatefulWidget {
-  
-  const MenuDrawer({super.key});
+  const MenuDrawer({Key? key});
 
   @override
   State<MenuDrawer> createState() => _MenuDrawerState();
 }
 
 class _MenuDrawerState extends State<MenuDrawer> {
-
   int indiceSeleccionado = 0;
 
   @override
   Widget build(BuildContext context) {
-
     final modoTrabajo = Provider.of<ModoTrabajo>(context);
-    final esAdmin=Provider.of<AdminProvider>(context);
-    
-    return  NavigationDrawer(
-      selectedIndex: indiceSeleccionado, // Permitirá cambiar el color del elemento seleccionado en el índice del menú
-      // El método onDestinationSelected se ejecutará cuando se seleccione un elemento del menú lateral y se le pasará el índice del elemento seleccionado
+    final esAdmin = Provider.of<AdminProvider>(context);
+
+    return NavigationDrawer(
+      selectedIndex: indiceSeleccionado,
       onDestinationSelected: (value) {
-        //setState es un método que permite actualizar el estado de un widget
         setState(() {
-          indiceSeleccionado = value; // Actualiza el índice seleccionado
+          indiceSeleccionado = value;
         });
 
-        // Navega a la ruta correspondiente al elemento seleccionado
         switch (value) {
           case 0:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => const PantallaInicial()),
-            ); 
+            );
             break;
           case 1:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (context) => const ListadoMedicamentos()),
-            ); 
+              MaterialPageRoute(
+                  builder: (context) => const ListadoMedicamentos()),
+            );
             break;
           case 2:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (context) => const ListadoTratamientos()),
-            ); 
+              MaterialPageRoute(
+                  builder: (context) => const ListadoTratamientos()),
+            );
             break;
           case 3:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (context) => const ListadoConsultasMedicas()),
+              MaterialPageRoute(
+                  builder: (context) => const ListadoConsultasMedicas()),
             );
             break;
           case 4:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => const InicioPantalla()),
             );
             break;
           case 5:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => const InicioPantalla()),
             );
             break;
           case 6:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (context) => const PaginaConfiguracion()),
+              MaterialPageRoute(
+                  builder: (context) => const PaginaConfiguracion()),
             );
             break;
           case 7:
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => const ListadoUsuarios()),
             );
             break;
-          default:
-            Navigator.pushReplacementNamed(context, '/'); 
+          
         }
-
       },
-      children:  [
-
-         Padding(
-          padding:  EdgeInsets.fromLTRB(28, 30, 16, 10), // EdgeInsets.fromLTRB(left, top, right, bottom)  sirve para definir el padding de un widget, muy útil para definir el espacio entre elementos
-         // Esto es un intento de crear un logo para la aplicación pero no funciona ??? Problema con los assets
-          child:  Row(
-          children: <Widget>[
-            Text(
-              'FarmacoFy',
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, 30, 16, 10),
+          child: Row(
+            children: <Widget>[
+              Text(
+                'FarmacoFy',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(width: 75),
+              CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage(
+                    'https://www.stylist.co.uk/images/app/uploads/2022/06/01105352/jennifer-aniston-crop-1654077521-1390x1390.jpg?w=256&h=256&fit=max&auto=format%2Ccompress'),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, 0, 28, 20),
+          child: Divider(
+            color: Colors.green,
+            thickness: 2,
+          ),
+        ),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.home), label: Text('Inicio')),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.medication_outlined), label: Text('Medicamentos')),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.medication_liquid_outlined),
+            label: Text('Tratamiento')),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.medical_services_outlined),
+            label: Text('Consultas médicas')),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.info_sharp), label: Text('Información')),
+        NavigationDrawerDestination(
+            icon: Icon(Icons.assignment_return_outlined),
+            label: Text('Cerrar sesion')),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, 10, 28, 20),
+          child: Divider(
+            color: Colors.green,
+            thickness: 2,
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(28, 0, 16, 10),
+            child: Text(
+              'Configuración',
               style: TextStyle(
-                color: Colors.green,
+                color: Colors.lightBlue,
                 fontSize: 24,
               ),
-            ),
-            // crear mas separacion del texto con el avatar
-            SizedBox(width: 75),
-          
-            // Image(image: AssetImage('assets/farmacofy.jpg'),
-            //   width: 60,
-            //   height: 60, 
-            //  ),
-          
-             CircleAvatar(
-
-            radius: 25,
-    
-            backgroundImage: NetworkImage(
-              
-                'https://www.stylist.co.uk/images/app/uploads/2022/06/01105352/jennifer-aniston-crop-1654077521-1390x1390.jpg?w=256&h=256&fit=max&auto=format%2Ccompress'
-                ),
+            )),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, 0, 28, 20),
+          child: Divider(
+            color: Colors.green,
+            thickness: 2,
           ),
- 
-              // Asegúrate de que la imagen esté en la carpeta de assets de tu proyecto
-          
-          ],
-        ),
-          ),
-
-            Padding(
-            padding: EdgeInsets.fromLTRB(28, 0, 28, 20),
-            child: Divider(
-              color: Colors.green,
-              thickness: 2,
-            ),
-          ),
-
-        // Cada elemento del menú lateral es un NavigationDrawerDestination
-        NavigationDrawerDestination(
-          icon:  Icon(Icons.home), 
-          label:  Text('Inicio')
         ),
         NavigationDrawerDestination(
-           
-            icon: Icon(Icons.medication_outlined), 
-            label: Text('Medicamentos')
-            ),
-       
-        NavigationDrawerDestination(
-          icon: Icon(Icons.medication_liquid_outlined), 
-          label: Text('Tratamiento')
-          ),
-        
-        NavigationDrawerDestination(
-          icon: Icon(Icons.medical_services_outlined), 
-          label: Text('Consultas médicas')
-          ),
-          
-        NavigationDrawerDestination(
-          icon:  Icon(Icons.info_sharp), 
-          label:  Text('Información')
-        ),
-         NavigationDrawerDestination(
-          icon:  Icon(Icons.assignment_return_outlined), 
-          label:  Text('Cerrar sesion')
-        ),
-         Padding(
-            padding: EdgeInsets.fromLTRB(28, 10, 28, 20),
-            child: Divider(
-              color: Colors.green,
-              thickness: 2,
-            ),
-          ),
-           Padding(
-          padding:  EdgeInsets.fromLTRB(28, 0, 16, 10), // EdgeInsets.fromLTRB(left, top, right, bottom)  sirve para definir el padding de un widget, muy útil para definir el espacio entre elementos
-          child: Text(
-            'Configuración',
-            style: TextStyle(
-              
-              color: Colors.lightBlue,
-              fontSize: 24,
-              //fontWeight: FontWeight.bold,
-            ),
-          )
-          ),
-            Padding(
-            padding: EdgeInsets.fromLTRB(28, 0, 28, 20),
-            child: Divider(
-              color: Colors.green,
-              thickness: 2,
-            ),
-          ),
-          NavigationDrawerDestination(
-          icon:  Icon(Icons.settings), 
-          label:  Text('Configuración')
-        ),
-        (esAdmin.esAdmin) ? NavigationDrawerDestination(
-          icon:  Icon(Icons.admin_panel_settings), 
-          label:  Text('Gestion de usuarios')
-        ) : Text("")
-          
-        
+            icon: Icon(Icons.settings), label: Text('Configuración')),
+        (esAdmin.esAdmin)
+            ? NavigationDrawerDestination(
+                icon: Icon(Icons.admin_panel_settings),
+                label: Text('Gestion de usuarios'))
+            : Text("")
       ],
     );
   }
